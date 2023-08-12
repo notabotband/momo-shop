6527bb37f0e236f262b717b71f142ad570a361af
antipovs <antipovs@yandex-team.ru>
2023-08-12 22:58:21 +0200

    Add deploy

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 docker network create -d bridge momo_network || true
-docker login gitlab.praktikum-services.ru:5050 -u $DOCKER_GITLAB_USER -p $DOCKER_GITLAB_TOCKEN
-docker pull gitlab.praktikum-services.ru:5050/std-013-59/momo-store/momo-backend:latest
+docker docker login -u $CI_REGISTRY_USER -p $CI_REGISTRY_PASSWORD $CI_REGISTRY
+docker pull $CI_REGISTRY_IMAGE/momo-backend:latest
 docker stop momo-backend || true
 docker rm momo-backend || true
 set -e
--- frontend/deploy.sh
+++ frontend/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 docker network create -d bridge momo_network || true
-docker login gitlab.praktikum-services.ru:5050 -u $DOCKER_GITLAB_USER -p $DOCKER_GITLAB_TOCKEN
-docker pull gitlab.praktikum-services.ru:5050/std-013-59/momo-store/momo-frontend:latest
+docker docker login -u $CI_REGISTRY_USER -p $CI_REGISTRY_PASSWORD $CI_REGISTRY
+docker pull $CI_REGISTRY_IMAGE/momo-frontend:latest
 docker stop momo-frontend || true
 docker rm momo-frontend || true
 set -e
