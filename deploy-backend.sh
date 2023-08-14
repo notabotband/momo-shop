a188e2bf6ab0ab9a16c4293fd69de6f3e29731c8
antipovs <antipovs@yandex-team.ru>
2023-08-14 16:42:09 +0200

    Fix

--- deploy-backend.sh
+++ deploy-backend.sh
@@ -2,11 +2,11 @@
 docker network create -d bridge momo_network || true
 docker login -u $CI_REGISTRY_USER -p $CI_REGISTRY_PASSWORD $CI_REGISTRY
 docker pull $CI_REGISTRY_IMAGE/momo-backend:latest
-docker stop momo-frontend || true
-docker rm momo-frontend || true
+docker stop momo-backend || true
+docker rm momo-backend || true
 set -e
-docker run -d --name momo-frontend \
+docker run -d --name momo-backend \
     --network=momo_network \
     --restart always \
     -p 81 \
-    gitlab.praktikum-services.ru:5050/std-013-59/momo-store/momo-frontend:latest
+    gitlab.praktikum-services.ru:5050/std-013-59/momo-store/momo-backend:latest
