06fcb854be8773c1a74a842f987a4714ff3f5789
antipovs <antipovs@yandex-team.ru>
2023-08-15 16:47:24 +0200

    Fix

--- deploy-compose.sh
+++ deploy-compose.sh
@@ -4,7 +4,7 @@ set -e
 sudo docker network create -d bridge momo_network || true
 sudo docker login -u $CI_REGISTRY_USER -p $CI_REGISTRY_PASSWORD $CI_REGISTRY
 sudo docker-compose pull
-sudo docker stop frontend || true
-sudo docker rm frontend || true
+sudo docker stop momo-frontend || true
+sudo docker rm momo-frontend || true
 set -e
-sudo docker-compose up -d frontend
+sudo docker-compose up -d momo-frontend
