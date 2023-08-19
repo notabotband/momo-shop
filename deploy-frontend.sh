ea2cdcd4d2b5202b51d8bfad020f2a81db933b54
antipovs <antipovs@yandex-team.ru>
2023-08-19 16:45:05 +0300

    Corrections based on comments

--- deploy-frontend.sh
+++ deploy-frontend.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
 set -e
-sudo docker network create -d bridge momo_network || true
 sudo docker login -u $CI_REGISTRY_USER -p $CI_REGISTRY_PASSWORD $CI_REGISTRY
 sudo docker-compose pull
-sudo docker stop momo-frontend || true
-sudo docker rm momo-frontend || true
-set -e
 sudo docker-compose up -d frontend
