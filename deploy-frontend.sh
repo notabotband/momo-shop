2eb903f9aa028726e51dc0e7af3c3c2951ac0061
antipovs <antipovs@yandex-team.ru>
2023-08-21 08:22:18 +0300

    DNS myvalue and letsencrypt

--- deploy-frontend.sh
+++ deploy-frontend.sh
@@ -3,4 +3,5 @@
 set -e
 sudo docker login -u $CI_REGISTRY_USER -p $CI_REGISTRY_PASSWORD $CI_REGISTRY
 sudo docker-compose pull
+sudo docker compose run --rm  certbot certonly --webroot --webroot-path /var/www/certbot/ -d momo.myvalue.ru
 sudo docker-compose up -d frontend
